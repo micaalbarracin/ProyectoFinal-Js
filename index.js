bc0062212c2c908c8d6aec408dbaf214c090206a
micaalbarracin <micaalbarracin@yahoo.com.ar>
2023-09-21 14:10:10 -0300

    cambios nuevos

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const baseDeDatos = [
         id: 1,
         nombre: 'Patata',
         precio: 1,
-        imagen: 'patata.jpg'
+        
     },
     {
         id: 2,
         nombre: 'Cebolla',
         precio: 1.2,
-        imagen: 'cebolla.jpg'
+        
     },
     {
         id: 3,
         nombre: 'Calabaza',
         precio: 2.1,
-        imagen: 'calabacin.jpg'
+        
     },
     {
         id: 4,
         nombre: 'Fresas',
         precio: 0.6,
-        imagen: 'fresas.jpg'
+        
     }
 
 ];
